77d0f14e72b7ef54d9c97a902281ba86e915cedf
gdochadipa <gdochadipa@gmail.com>
2020-07-28 11:06:55 +0800

    selasa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,40 @@
     Home
 @endsection
 @section('component')
+<style>
+  .btn{
+    border-radius: 0;
+  }
+  .carousel-content {
+  position: absolute;
+  top: 40%;
+  left: 15%;
+  z-index: 20;
+  color: white;
+  text-shadow: 0 1px 2px rgba(0,0,0,.6);
+}
+.danger{
+  color: antiquewhite;
+  background-color:  #ED1B24;
+}
+</style>
 <main role="main">
 <br>
 <br>
 <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="{{asset('asset/slide.png')}}" class="d-block w-100" alt="...">
+       <img src="{{asset('asset/slide.png')}}" class="d-block w-100" alt="...">
+       
+      
     </div>
     <div class="carousel-item">
       <img src="{{asset('asset/slide.png')}}" class="d-block w-100" alt="...">
+      
     </div>
     <div class="carousel-item">
       <img src="{{asset('asset/slide.png')}}" class="d-block w-100" alt="...">
+      
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -26,6 +47,10 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
+  <div class="carousel-content">
+        <h1 style="font-size: 75px">Welcome To <span class="text-info">WBF</span></h1>
+        <p class="p-1 danger text-white" style="width: 60%;"><i>WORLD BRAND FASION, KUTA - BALI</i></p>
+      </div>
 </div>
 <div class="container py-5 h-100">
   <div class="row text-center justify-content-center">
@@ -148,10 +173,21 @@
       </div>
       <div class="col">
        <div class="float-right align-middle">
-          <button type="button" class="btn btn-danger btn-lg">GO TO SALE <i class="fa fa-arrow-right" style="color:white" aria-hidden="true"></i></button>
+          <button type="button" class="btn danger btn-lg">GO TO SALE <i class="fa fa-arrow-right" style="color:white" aria-hidden="true"></i></button>
        </div>
       </div>
     </div>
+</div>
+
+<div class="container py-5 h-100">
+  <div class="row text-center justify-content-center">
+    <h1 class="">
+      FOR YOU
+    </h1>
+  </div>
+  <div class="row text-center justify-content-center">
+    <img src="{{asset('asset/red-line.png')}}" alt="">
+  </div>
 </div>
 
   <div class="album py-5 bg-light">
@@ -298,6 +334,30 @@
       </div>
     </div>
   </div>
+
+<div class="container py-5 h-100">
+  <div class="row text-center justify-content-center">
+     <button type="button" class="btn danger btn-lg" style="width: 25%; height:30%"><span style="font-size: 15px"> SEE ALL PRODUCT  </span> </button>
+  </div>
+  <div class="row text-center justify-content-center">
+    
+  </div>
+</div>
+
+<div style="margin: 15px 0; background-image: url('{{asset('asset/banner2.png')}}');" class="img-fluid w-100 p-3 h-100 d-inline-block" >
+<div class="container py-5 h-100">
+  <div class="row text-center justify-content-center">
+    <h1 style="color: antiquewhite">
+      BRANDS
+    </h1>
+  </div>
+  <div class="row text-center justify-content-center">
+    <img src="{{asset('asset/red-line.png')}}" alt="">
+  </div>
+</div>
+</div>
+
+
 
 </main>
 
